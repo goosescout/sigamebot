f00085c7b1e005194da06a54ce555f490e2f0232
goosescout <michael.gurevich.02@mail.ru>
2020-04-26 22:19:43 +0700

    Sending picture

--- test.py
+++ test.py
@@ -5,6 +5,7 @@ import pymorphy2
 
 from dotenv import load_dotenv
 from discord.ext import commands
+from PIL import Image, ImageDraw, ImageFont
 
 
 load_dotenv()
@@ -30,7 +31,7 @@ class SiGameBot(commands.Bot):
         categories = cur_game.get_categories()
         str_categories = '\n'.join(map(lambda x: '• ' + x[0] + ' - ' + x[1], categories.items()))
         await channel.send(f"Начинается {cur_round} раунд\n" +
-                           f"Категории раунда:\n{str_categories}")
+                           f"Категории раунда:\n{str_categories}", file=discord.File(cur_game.get_image_path()))
  
 
 class SiCommands(commands.Cog):
@@ -43,7 +44,7 @@ class SiCommands(commands.Cog):
             await ctx.send("Игра в этом канале уже началась")
             return
         try:
-            self.bot.games[ctx.message.channel] = GameSession(pack_name)
+            self.bot.games[ctx.message.channel] = GameSession(pack_name, ctx.message.channel.id)
         except FileNotFoundError:
             await ctx.send("Пак не найден")
         else:
@@ -53,7 +54,7 @@ class SiCommands(commands.Cog):
 
     @commands.command(name='join')
     async def join(self, ctx):
-        if ctx.channel in self.bot.games.keys() and self.bot.games[ctx.channel].is_joinable():
+        if ctx.channel in self.bot.games.keys():
             try:
                 self.bot.games[ctx.channel].add_member(ctx.message.author)
             except Exception as ex:
@@ -80,11 +81,14 @@ class SiCommands(commands.Cog):
     async def on_command_error(self, ctx, error):
         if isinstance(error, commands.CommandNotFound):
             await ctx.send(f"Такой команды не существует")
+        else:
+            await ctx.send(f"{error} (фича для дебага. В дальнейшем такого не будет)")
 
 
 class GameSession:
-    def __init__(self, pack_name):
+    def __init__(self, pack_name, game_id):
         self.pack_name = pack_name
+        self.id = game_id
         # здесь будет получение пака через API
         
         with open(f'{pack_name}.json') as f:
@@ -98,10 +102,13 @@ class GameSession:
         self.cur_round = 0
 
     def add_member(self, member):
-        if member in self.members.keys():
-            raise ValueError(f"{member.mention}, ты уже в игре!")
+        if self.joinable:
+            if member in self.members.keys():
+                raise ValueError(f"{member.mention}, ты уже в игре!")
+            else:
+                self.members[member] = 0
         else:
-            self.members[member] = 0
+            raise ValueError('Сбор игроков уже завершен')
 
     def is_joinable(self):
         return self.joinable
@@ -115,9 +122,12 @@ class GameSession:
                 self.joinable = False
             else:
                 raise ValueError('Нельзя начать игру, в которой нет игроков')
+        else:
+            raise ValueError('Сбор игроков уже завершен')
 
     def update_round(self):
         self.cur_round += 1
+        self.create_table_image()
         return self.cur_round
 
     def get_categories(self):
@@ -125,6 +135,24 @@ class GameSession:
         for category in self.pack['rounds'][self.cur_round - 1]['categories']:
             result[category['name']] = category['description']
         return result
+
+    def create_table_image(self):
+        img = Image.new("RGB", (701, len(self.pack['rounds'][self.cur_round - 1]['categories']) * 75 + 1), (0, 0, 255))
+        draw = ImageDraw.Draw(img)
+        font = ImageFont.truetype('static/OpenSans.ttf', 35)
+        for i in range(6):
+            for j in range(len(self.pack['rounds'][self.cur_round - 1]['categories'])):
+                if i == 0:
+                    draw.rectangle([(i * 100, j * 75), ((i + 1) * 100 + 100, (j + 1) * 75)], width=2, outline=(255, 255, 0))
+                    draw.text((i * 100 + 5, j * 75 + 15), self.pack['rounds'][self.cur_round - 1]['categories'][j]['name'], fill=(255, 255, 0), font=font)
+                else:
+                    draw.rectangle([(i * 100 + 100, j * 75), ((i + 1) * 100 + 100, (j + 1) * 75)], width=2, outline=(255, 255, 0))
+                    draw.text((i * 100 + 115, j * 75 + 15), str(self.pack['rounds'][self.cur_round - 1]['categories'][j]['questions'][i - 1]['par']), fill=(255, 255, 0), font=font)
+
+        img.save(f'temp/{self.id}.png')
+
+    def get_image_path(self):
+        return f'temp/{self.id}.png'
 
 
 bot = SiGameBot(command_prefix='si! ')
